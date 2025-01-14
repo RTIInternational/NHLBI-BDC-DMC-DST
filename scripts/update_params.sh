f8299ecc794e92aba2971c036cc47457a657ada0
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-01-13 21:35:02 -0500

    Updated.

--- scripts/update_params.sh
+++ scripts/update_params.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp scripts/update_env.py /home/ubuntu/dst-pipeline/scripts
-cp api/.env.sample /home/ubuntu/dst-pipeline/scripts/api/.env
+#cp scripts/update_env.py /home/ubuntu/dst-pipeline/scripts
+#cp api/.env.sample /home/ubuntu/dst-pipeline/scripts/api/.env
 cd /home/ubuntu/dst-pipeline/scripts
 
 python3 update_env.py
