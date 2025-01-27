c2b4ea3c73b5cfdda36439a6a9f5b91404d4bce0
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-01-27 17:23:29 -0500

    Updated.

--- scripts/update_env.py
+++ scripts/update_env.py
@@ -54,9 +54,13 @@ def update_env_file(parameters):
 def main():
     # List of parameter names to fetch from SSM
     parameter_names = [
+        "/dst/dev/SECRET_KEY",
         "/dst/dev/POSTGRES_DB",
+        "/dst/dev/POSTGRES_USER",
         "/dst/dev/POSTGRES_PASSWORD",
-        "/dst/dev/POSTGRES_USER"    
+        "/dst/dev/JIRA_TOKEN",
+        "/dst/dev/FRESHDESK_AUTH_USER",
+        "/dst/dev/FRESHDESK_AUTH_PASSWORD",
     ]
 
     # Fetch parameters from SSM Parameter Store
