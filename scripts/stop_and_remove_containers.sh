3305ed0691b6b87d29549aefd16a914fe49efa8e
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-01-27 18:29:12 -0500

    Stop, remove and delete the app image and container

--- scripts/stop_and_remove_containers.sh
+++ scripts/stop_and_remove_containers.sh
@@ -1,5 +1,9 @@
 # Stop and remove only the dst app container
 
-docker rm -f bdc-dashboard-app
+#docker rm -f bdc-dashboard-app
+
+# Force Stop, Remove, and Delete Image
+docker stop bdc-dashboard-app && docker rm <bdc-dashboard-app && docker rmi $(docker inspect bdc-dashboard-app --format '{{.Image}}')
+
 
 
