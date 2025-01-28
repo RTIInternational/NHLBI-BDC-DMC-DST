d1275e4bc3805b96e7ce0e886f55a6684b946fb3
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-01-27 19:04:59 -0500

    Update start_containers.sh

--- scripts/start_containers.sh
+++ scripts/start_containers.sh
@@ -3,5 +3,6 @@ docker load < /home/ubuntu/dst-pipeline/nhlbi-bdc-dmc-dst_bdc-dashboard-app.tar
 
 #!/bin/bash
 cd /home/ubuntu/dst-pipeline/scripts
+
 # Build and start the containers
-docker-compose up --biuld
+docker-compose up -d
