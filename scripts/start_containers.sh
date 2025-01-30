aaa9fdff13d0e09ba2d65eb44ca8a4112c860b56
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-01-30 12:05:22 -0500

    Added back the db container.

--- scripts/start_containers.sh
+++ scripts/start_containers.sh
@@ -5,4 +5,4 @@ docker load < /home/ubuntu/dst-pipeline/nhlbi-bdc-dmc-dst_bdc-dashboard-app.tar
 cd /home/ubuntu/dst-pipeline/scripts
 
 # Build and start the containers
-docker-compose up -d
+docker-compose up -d bdc-dashboard-app
