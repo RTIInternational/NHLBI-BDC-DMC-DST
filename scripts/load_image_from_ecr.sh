4559261bfd4aa5bcabccaab7821d99d0ab46f3f1
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-02-07 14:45:57 -0500

    Updated to fix pulling of the ECR image.

--- scripts/load_image_from_ecr.sh
+++ scripts/load_image_from_ecr.sh
@@ -6,9 +6,10 @@ ECR_REPO="099963124704.dkr.ecr.us-east-1.amazonaws.com/dst/nhlbi-bdc-dmc-dst_bdc
 IMAGE_TAG="latest"  # Change tag if necessary
 
 echo "Logging into Amazon ECR..."
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPO
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 099963124704.dkr.ecr.us-east-1.amazonaws.com
 
 echo "Pulling latest Docker image from ECR..."
-docker pull $ECR_REPO:$IMAGE_TAG
+#docker pull $ECR_REPO:$IMAGE_TAG
 
 
+docker pull 099963124704.dkr.ecr.us-east-1.amazonaws.com/dst/nhlbi-bdc-dmc-dst_bdc-dashboard-app:latest
