71ac05a0031d52147fdfc64d8a1a9a8d45102e17
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-02-09 16:12:06 -0500

    Updated

--- scripts/start_containers.sh
+++ scripts/start_containers.sh
@@ -1,6 +1,7 @@
 #The command never executed in the Install phase for some reason
 docker load < /home/ubuntu/dst-pipeline/nhlbi-bdc-dmc-dst_bdc-dashboard-app.tar
 
+
 #!/bin/bash
 cd /home/ubuntu/dst-pipeline/scripts
 
