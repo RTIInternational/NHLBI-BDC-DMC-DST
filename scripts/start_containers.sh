1d991052595763831d3c2bb57737a2a5e8821aa6
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-02-09 14:23:06 -0500

    Update start_containers.sh

--- scripts/start_containers.sh
+++ scripts/start_containers.sh
@@ -1,6 +1,9 @@
 #The command never executed in the Install phase for some reason
 #docker load < /home/ubuntu/dst-pipeline/nhlbi-bdc-dmc-dst_bdc-dashboard-app.tar
 
+echo "Logging into Amazon ECR..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 099963124704.dkr.ecr.us-east-1.amazonaws.com
+
 #!/bin/bash
 cd /home/ubuntu/dst-pipeline/scripts
 
