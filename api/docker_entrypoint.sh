ded32dfaa660ed903706c8c58549ac8bbcccff4a
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-02-13 09:45:57 -0500

    Iteration

--- api/docker_entrypoint.sh
+++ api/docker_entrypoint.sh
@@ -11,10 +11,6 @@ python3 manage.py makemigrations
 echo "Apply database migrations"
 python3 manage.py migrate
 
-# Address vulnarebilities findings
-echo "Upgrade certificate"
-pip install --upgrade certifi
-
 # Start server
 echo "Starting server"
 python3 manage.py runserver 0.0.0.0:8000
