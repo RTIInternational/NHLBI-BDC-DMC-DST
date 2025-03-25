b1a91a87c75a7c4aa03014343451019e3e000a32
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-03-25 12:26:18 -0400

    Pushed changes from deploy-aws-dev branch.

--- api/docker_entrypoint.sh
+++ api/docker_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-export PATH="/.pyenv/bin:${PATH}"; \
-eval "$(pyenv init -)"; \
+# Activate virtual environment
+source /opt/venv/bin/activate
 
 # Apply database migrations
 echo "Apply database migrations"
@@ -10,6 +10,9 @@ python3 manage.py makemigrations
 # Apply database migrations
 echo "Apply database migrations"
 python3 manage.py migrate
+
+# Create super user
+python3 create_superuser.py
 
 # Start server
 echo "Starting server"
