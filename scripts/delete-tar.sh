d62d24e36f8e21ed379ad2a89ce23fa9ad3a6080
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-04-20 15:18:28 -0400

    Renamed to keep the convention with the rest of the scripts.

--- scripts/delete-tar.sh
+++
@@ -1,3 +0,0 @@
-echo "Deleting code tar file..."
-cd /home/ubuntu/dst-pipeline
-sudo rm nhlbi-bdc-dmc-dst_bdc-dashboard-app.tar
---
+++ scripts/delete_tar.sh
@@ -0,0 +1,2 @@
+echo "Deleting code tar file..."
+sudo rm /home/ubuntu/dst-pipeline/nhlbi-bdc-dmc-dst_bdc-dashboard-app.tar
