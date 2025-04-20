2c30db5d7d98ba3cc9d9448e0ca94882ff933ed0
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-04-20 15:39:32 -0400

    Updated to resolve the permission issue.

--- scripts/update_params.sh
+++ scripts/update_params.sh
@@ -4,7 +4,7 @@
 cd /home/ubuntu/dst-pipeline/scripts
 
 cp api/.env.sample /home/ubuntu/dst-pipeline/scripts/api/.env
-
+sudo chown ubuntu -R /home/ubuntu/dst-pipeline/scripts/api
 python3 update_env.py
 
 chmod  664 /home/ubuntu/dst-pipeline/scripts/api/.env
