16d08ede583c4d6adf5fd3c78b68425a9d67be78
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-04-30 21:44:42 -0400

    Updated to work with the dev tar
    Updated to work with the dev tar

--- scripts/copy_from_s3.sh
+++ scripts/copy_from_s3.sh
@@ -4,9 +4,9 @@
 
 
 #!/bin/bash
-sudo rm /home/ubuntu/dst-pipeline/nhlbi-bdc-dmc-dst_bdc-dashboard-app.tar
+sudo rm /home/ubuntu/dst-pipeline/nhlbi-bdc-dmc-dst_bdc-dashboard-app-dev.tar
 
-aws s3 cp s3://bdc-dmc-dst-dashboard-app/nhlbi-bdc-dmc-dst_bdc-dashboard-app.tar /home/ubuntu/dst-pipeline
+aws s3 cp s3://bdc-dmc-dst-dashboard-app/nhlbi-bdc-dmc-dst_bdc-dashboard-app-dev.tar /home/ubuntu/dst-pipeline
 
 
 
