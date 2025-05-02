5eac08da225ddc429a1ccdd6d18f1afba3d9e59c
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-05-02 17:17:38 -0400

    Updated to handle the test env. params

--- scripts/update_params.sh
+++ scripts/update_params.sh
@@ -5,6 +5,6 @@ cd /home/ubuntu/dst-pipeline/scripts
 
 cp api/.env.sample /home/ubuntu/dst-pipeline/scripts/api/.env
 sudo chown ubuntu -R /home/ubuntu/dst-pipeline/scripts/api
-python3 update_env.py
+python3 update_env.py test
 
 chmod  664 /home/ubuntu/dst-pipeline/scripts/api/.env
